26b945522f05056c598f94a895f887817044d989
Deividson Sabino <33181693+deivaotv@users.noreply.github.com>
2022-02-02 20:42:44 -0300

    feat: autenticação-com-Next-Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,26 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const { data: session } = useSession();
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Deivid Sabino
-            <FiX color="#737380" className={styles.closeIcon}/>
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
-            className={styles.signInButton}
+                className={styles.signInButton}
+                onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
 
