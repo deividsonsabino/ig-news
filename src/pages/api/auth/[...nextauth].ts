ffde9a5782c0c729bce9a14ce7d4ce0d1d3d0ac8
Deividson Sabino <33181693+deivaotv@users.noreply.github.com>
2022-02-03 23:54:41 -0300

    feat: verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,16 +17,32 @@ export default NextAuth({
             }
         }),
     ],
-    
+
     callbacks: {
         async signIn({ user, account, profile }) {
             const { email } = user
 
             try {
                 await fauna.query(
-                    q.Create(
-                        q.Collection('users'),
-                        { data: { email } }
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
                     )
                 )
 
